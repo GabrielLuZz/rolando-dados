3f418805bdfe94876c1b9f88d6ddf62a4da9bf00
Gabriel Luz <gabrielluzsantosl@gmail.com>
2022-03-09 16:10:19 -0300

    Primeira tentativa de entrega

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
+let dado1;
+let dado2;
+let lancamento = [];
+let frequencia;
+let frequenciaArray = [];
 
+
+for (let i = 1; i <= 1000; i++) {
+    dado1 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+    dado2 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+
+    let soma = dado1 + dado2;
+
+    lancamento.push(soma);
+}
+
+frequencia = lancamento.reduce((a, b) => {
+    if (a[b]) {
+        a[b]++;
+    } else {
+        a[b] = 1
+    }
+
+    return a;
+}, {})
+
+for (let index in frequencia) {
+    frequenciaArray.push(frequencia[index]);
+}
+
+let divs = document.querySelectorAll('li div');
+let botao = document.getElementById('rolar');
+let img = document.querySelector('#demonstracao img');
+
+function rolar() {
+    for (let i = 0; i < frequenciaArray.length; i++) {
+        divs[i].style.width = 2 + 'px';
+    }
+    img.setAttribute('src', 'images/dados.webp');
+
+    setTimeout(() => {
+        img.setAttribute('src', 'images/parados.png')
+
+        for (let i = 0; i < frequenciaArray.length; i++) {
+            divs[i].style.width = frequenciaArray[i] * 3 + 'px';
+        }
+    }, 4000)
+}
+
+
+botao.addEventListener('click', rolar)
